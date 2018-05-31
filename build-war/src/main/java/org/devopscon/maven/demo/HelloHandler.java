ffed034f77345805a05e7591734f21cb2472e6f5
arminkunaschik <39690965+arminkunaschik@users.noreply.github.com>
2018-05-31 16:36:49 +0200

    plural!

--- build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
+++ build-war/src/main/java/org/devopscon/maven/demo/HelloHandler.java
@@ -3,7 +3,7 @@ package org.devopscon.maven.demo;
 public class HelloHandler {
 
 	public String sayHello() {
-		String hello = "Hello DevOpsCon workshop member, What do you think about this WAR file?";
+		String hello = "Hello DevOpsCon workshop members, What do you think about this WAR file?";
 		return hello;
 	}
 
